7a0d845d3d035ada6f189ca3fe4228b4649aa01a
Ori <disori777@gmail.com>
2024-11-10 09:23:32 +0800

    A

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template
 
 app = Flask("Website")
 
-@app.route("/home")
+@app.route("/")
 def home():
     return render_template("tutorial.html")
+
+
+app.run(debug=True)
