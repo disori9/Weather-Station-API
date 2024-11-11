f60a86074b468de51dd59322b503c31becd859e9
Ori <disori777@gmail.com>
2024-11-11 21:59:10 +0800

    Implement API url that shows the temperature of a specific station in its specific date

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+import pandas as pd
+import numpy as np
+
 
 app = Flask(__name__)
 
@@ -9,11 +12,16 @@ def home():
 
 
 @app.route("/api/v1/<station>/<date>")
+
 def temp(station, date):
-    temperature = 23
+    filename = f"data_small/TG_STAID{str(station).zfill(6)}.txt"
+    temperature_df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    temperature_df["TG0"] = temperature_df['   TG'].mask(temperature_df['   TG'] == -9999, np.nan)
+    temperature_df["TG"] = temperature_df["TG0"] / 10
+    date_temp = temperature_df.loc[temperature_df['    DATE'] == date]['TG'].squeeze()
     return {"station": station,
             "date": date,
-            "temperature": temperature}
+            "temperature": date_temp}
 
 
 if __name__ == "__main__":
